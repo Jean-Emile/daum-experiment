c7523e3ee43f57069d50ca39265f30f8ca27d29e
JeD <jedartois@gmail.com>
2013-01-31 13:24:33 +0100

    clean

--- org.kevoree.library.javase.fakeDataProvider/target/generated-sources/kevoree/org/kevoree/library/javase/trustStuff/kevgen/JavaSENode/TrustManagerMoverActivator.scala
+++
@@ -1,9 +0,0 @@
-package org.kevoree.library.javase.trustStuff.kevgen.JavaSENode;
-import java.util.Hashtable
-import org.kevoree.api.service.core.handler.KevoreeModelHandlerService
-import org.kevoree.framework.KevoreeActor
-import org.kevoree.framework.KevoreeComponent
-import org.kevoree.framework._
-import org.kevoree.library.javase.trustStuff._
-class TrustManagerMoverActivator extends org.kevoree.framework.osgi.KevoreeComponentActivator {
-def callFactory() : KevoreeComponent = { TrustManagerMoverFactory.createComponentActor() } }
